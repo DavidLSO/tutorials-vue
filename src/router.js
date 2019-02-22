45dc014d950f65c30a49fc270ea996416b8178ea
David Levy Sousa Oliveira <jbdeivin@gmail.com>
2019-02-21 22:49:03 -0300

    first commit to first tutorial

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import One from "./views/One.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/one",
+      name: "one",
+      component: One
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});
---
+++ src/views/One.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <b-container>
+      <b-row class="shadow-lg p-3 mb-5 bg-white rounded">
+        <b-col md="6">
+          <p>Player</p>
+          <b-progress
+            :max="max"
+            :value="valuePlayer"
+            :variant="colorBarPlayer"
+            show-progress
+            animated
+          ></b-progress>
+        </b-col>
+        <b-col md="6">
+          <p>Monstro</p>
+          <b-progress
+            :max="max"
+            :value="valueMonster"
+            :variant="colorBarMonster"
+            show-progress
+            animated
+          ></b-progress>
+        </b-col>
+      </b-row>
+      <b-row class="shadow-lg p-3 mb-5 bg-white rounded">
+        <b-col md="12">
+          <b-button-group>
+            <b-button variant="danger" >Ataque</b-button>
+            <b-button variant="warning" >Ataque Especial</b-button>
+            <b-button variant="success" >Curar</b-button>
+            <b-button>Desistir</b-button>
+          </b-button-group>
+        </b-col>
+      </b-row>
+      <b-row class="shadow-lg p-3 mb-5 bg-white rounded">
+        <b-col md="12">
+          <b-alert variant="success" show>Success Alert</b-alert>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      max: 100,
+      valuePlayer: 100,
+      valueMonster: 100,
+      colorBarPlayer: "success",
+      colorBarMonster: "success"
+    };
+  }
+};
+</script>
+
+<style></style>
